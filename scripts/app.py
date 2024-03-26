953713edd9ce31ee6f3f11764c18ab332f179e9f
JosueJNLui <nlui.josuej@gmail.com>
2024-03-26 11:14:04 -0300

    Creating the web interface for the project

--- scripts/app.py
+++ scripts/app.py
@@ -1,9 +1,35 @@
 import streamlit as st
+import pandas as pd
 
 def data_upload():
+
     extensios_allowed = ["csv", "xls", "xlsx"]
     data = st.file_uploader("Down below upload your data file", type=extensios_allowed)
 
+    return data
+
+def read_data(data) -> pd.DataFrame:
+
+    if data:
+
+        if data.name.endswith('csv'):
+            df = pd.read_csv(data)
+        elif data.name.endswith('xls') | data.name.endswith('xlsx'):
+            df = pd.read_excel(data)
+        else:
+            df = None
+        return df
+
+def print_dataframe(df: pd.DataFrame):
+
+    if df is not None:
+        st.dataframe(df)
+
+def dataframe_check(df: pd.DataFrame):
+
+    if df is not None:
+        check = st.button("Reset")
+        return check
 
 st.set_page_config(page_title="Data Viz generation using prompt")
 
@@ -18,4 +44,7 @@ with st.container():
 
 with st.container():
     st.subheader("Upload your Data")
-    data_upload()
+    data = data_upload()
+    df = read_data(data)
+    st.subheader("Data upload's Head")
+    print_dataframe(df)
