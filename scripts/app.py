f19e790f31e1481c42df5d6c86dc8be1ac985798
JosueJNLui <nlui.josuej@gmail.com>
2024-03-27 08:51:27 -0300

    Adding some blocks of codes to be able to use the pandasai + openai

--- scripts/app.py
+++ scripts/app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
+import os
+from dotenv import load_dotenv
 
 def data_upload():
 
@@ -25,11 +29,7 @@ def print_dataframe(df: pd.DataFrame):
     if df is not None:
         st.dataframe(df)
 
-def dataframe_check(df: pd.DataFrame):
-
-    if df is not None:
-        check = st.button("Reset")
-        return check
+load_dotenv()
 
 st.set_page_config(page_title="Data Viz generation using prompt")
 
@@ -48,3 +48,15 @@ with st.container():
     df = read_data(data)
     st.subheader("Data upload's Head")
     print_dataframe(df)
+
+with st.container():
+    st.subheader("Prompt to get what you desire")
+    prompt = st.text_input('Specify in the text box below what do you want')
+    if prompt is not None and df is not None:
+        llm = OpenAI(api_token=os.environ["OPENAI_API_KEY"])
+        sdf = SmartDataframe(df, config={"llm":llm})
+        response = sdf.chat(prompt)
+        st.write(f"""
+        {response}
+        """)
+    
